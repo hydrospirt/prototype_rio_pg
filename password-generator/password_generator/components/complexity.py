3aec8f9214cb1ad15848ae10f4c9e77119b9acf9
Eduard Gumen <hydrospirt@mail.ru>
2024-08-02 22:47:13 +0300

    Change Icon and add bits text

--- password-generator/password_generator/components/complexity.py
+++ password-generator/password_generator/components/complexity.py
@@ -2,19 +2,35 @@ import rio
 
 
 class Complexity(rio.Component):
+    hex_color: str = "#ff0000"
 
     def build(self) -> rio.Component:
         return rio.Grid(
-            rio.Row(rio.Text(text='Сложность:',
+            rio.Row(rio.Text(text="Сложность:",
                              selectable=False,
                              style=rio.TextStyle(
                                 font_weight="bold"
                              )),
-                    rio.Banner(text='Пусто',
-                               style='info'),
-                    rio.Text(text='Энтропия:',
-                             selectable=False),
-                    rio.Icon(icon='material/encrypted:fill',
+                    rio.Banner(text="Пусто",
+                               style="warning",
+                               align_x=-0.3,
+                               width=0.5),
+                    rio.Icon(icon="material/encrypted:fill",
+                             align_x=-0.5,
+                             width=2,
+                             height=2,
                              fill=rio.LinearGradientFill(
                                  (self.session.theme.secondary_color, 0),
-                                 (self.session.theme.primary_color, 1),),),))
+                                 (self.session.theme.primary_color, 1),),),
+                    rio.Text(text="Энтропия:",
+                             selectable=False,
+                             align_x=-0.8,
+                             style=rio.TextStyle(
+                                font_weight="bold")),
+                    rio.Text(text="0 bits",
+                             selectable=False,
+                             align_x=-1.6,
+                             style=rio.TextStyle(
+                                font_weight="bold",
+                                underlined=True,
+                                fill=rio.Color.from_hex(self.hex_color)),)))
