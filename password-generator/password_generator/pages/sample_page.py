9e2bfe174a5e3a5a162efa119fbafcba98c25e94
Eduard Gumen <hydrospirt@mail.ru>
2024-08-02 21:43:23 +0300

    Initial commit / add Title and GenPlaceholder

---
+++ password-generator/password_generator/pages/sample_page.py
@@ -0,0 +1,19 @@
+import rio
+
+from .. import components as comps
+
+
+class SamplePage(rio.Component):
+    """
+    The main page of the password generator
+    """
+
+    def build(self) -> rio.Component:
+        return rio.Column(
+            comps.Title(),
+            comps.GenPlaceholder(),
+            spacing=2,
+            margin=2,
+            align_x=0.5,
+            align_y=0,
+        )
