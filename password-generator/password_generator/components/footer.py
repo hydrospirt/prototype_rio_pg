4dcc6177b2735dc1f2bbab64441053eb3a6b3914
Eduard Gumen <hydrospirt@mail.ru>
2024-08-03 23:32:04 +0300

    Add links to footer: phone, email, adress

--- password-generator/password_generator/components/footer.py
+++ password-generator/password_generator/components/footer.py
@@ -1,15 +1,26 @@
-from __future__ import annotations
-
-from dataclasses import KW_ONLY, field
-from typing import *  # type: ignore
-
+from datetime import datetime as dt
 import rio
 
-from .. import components as comps
+year = dt.now().year
 
 
 class Footer(rio.Component):
-    example_state: str = "For demonstration purposes"
+    copyright: str = (f"© 2007 — {year} ООО «Роксис». "
+                      + "Обслуживание компьютеров организаций "
+                      + "в Зеленограде и Москве.")
+    target_link: str = "https://www.roksis.ru/contacts/"
+    adress: str = "Москва, Зеленоград, ул. Юности, д. 8, оф. 702-718"
+    email: str = "info@roksis.ru"
+    phone: str = "8 (495) 662-47-21"
 
     def build(self) -> rio.Component:
-        return rio.Text(self.example_state)
+        return rio.Grid(
+            rio.Text(self.copyright),
+            rio.Row(
+                rio.Link(content=self.phone,
+                         target_url=self.target_link),
+                rio.Link(content=self.email,
+                         target_url=self.target_link),
+                rio.Link(content=self.adress, target_url=self.target_link),),
+            row_spacing=0.5,
+        )
