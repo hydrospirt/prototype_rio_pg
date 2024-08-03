108971c42d2dbd5baf32bf51acd40aaf9d41cd88
Eduard Gumen <hydrospirt@mail.ru>
2024-08-03 23:45:32 +0300

    Add delete_bad_symbols and BAD_SYMBOLS

--- password-generator/password_generator/utils/utils.py
+++ password-generator/password_generator/utils/utils.py
@@ -6,6 +6,7 @@ LOWERCASE = ascii_lowercase
 UPPERCASE = ascii_uppercase
 DIGITS = digits
 PUNCTUATION = punctuation
+BAD_SYMBOLS = "liLI"
 
 
 def create_new_password(pass_len: int, char: str) -> str:
@@ -14,3 +15,7 @@ def create_new_password(pass_len: int, char: str) -> str:
 
 def get_password_entropy(pass_len: int, char_num: str) -> float:
     return round((pass_len * log2(char_num)), 1)
+
+
+def delete_bad_symbols(ascii_symbols: str) -> str:
+    return ascii_symbols.translate({ord(i): None for i in BAD_SYMBOLS})
