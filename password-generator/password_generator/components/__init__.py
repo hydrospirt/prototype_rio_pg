4f845b047a9f9712cc97ad448e309fb59e8dc64d
Eduard Gumen <hydrospirt@mail.ru>
2024-08-03 13:37:39 +0300

    Add GlobalComplexity, fix import

--- password-generator/password_generator/components/__init__.py
+++ password-generator/password_generator/components/__init__.py
@@ -1,6 +1,5 @@
 
 from .gen_placeholder import GenPlaceholder # noqa
-from .options import Options # noqa
-from .options import GlobalOptions # noqa
-from .complexity import Complexity # noqa
+from .options import Options, GlobalOptions # noqa
+from .complexity import Complexity, GlobalComplexity # noqa
 from .title import Title # noqa
