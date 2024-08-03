81319be09a47070788a7a67500843b247c8a857d
Eduard Gumen <hydrospirt@mail.ru>
2024-08-03 13:23:32 +0300

    Add GlobalOptions

--- password-generator/password_generator/components/__init__.py
+++ password-generator/password_generator/components/__init__.py
@@ -1,5 +1,6 @@
 
 from .gen_placeholder import GenPlaceholder # noqa
 from .options import Options # noqa
+from .options import GlobalOptions # noqa
 from .complexity import Complexity # noqa
 from .title import Title # noqa
