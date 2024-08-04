38939f8263afe6f53cdaf7dda4c170eeab7db765
Eduard Gumen <hydrospirt@mail.ru>
2024-08-04 16:05:37 +0300

    Add __post_init__

--- password-generator/password_generator/components/options.py
+++ password-generator/password_generator/components/options.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 
 import rio
 
+from ..utils import Symbols, delete_bad_symbols, delete_symbols
+
 
 @dataclass
 class GlobalOptions:
@@ -12,23 +14,75 @@ class GlobalOptions:
     is_on_nums: bool = True
     is_on_symbols: bool = False
     is_on_bad_symbol: bool = True
+    symbols: str = ""
 
 
 class Options(rio.Component):
+    def __post_init__(self):
+        if GlobalOptions.is_on_lower:
+            if (Symbols.LOWERCASE not in GlobalOptions.symbols
+                    and Symbols.CLEAR_LOWERCASE not in GlobalOptions.symbols):
+                GlobalOptions.symbols += Symbols.LOWERCASE
+        if GlobalOptions.is_on_upper:
+            if (Symbols.UPPERCASE not in GlobalOptions.symbols
+                    and Symbols.CLEAR_UPPERCASE not in GlobalOptions.symbols):
+                GlobalOptions.symbols += Symbols.UPPERCASE
+        if GlobalOptions.is_on_nums:
+            if Symbols.DIGITS not in GlobalOptions.symbols:
+                GlobalOptions.symbols += Symbols.DIGITS
+        if GlobalOptions.is_on_symbols:
+            if Symbols.PUNCTUATION not in GlobalOptions.symbols:
+                GlobalOptions.symbols += Symbols.PUNCTUATION
+        if GlobalOptions.is_on_bad_symbol:
+            GlobalOptions.symbols = delete_bad_symbols(GlobalOptions.symbols)
+
     def is_on_change_lower(self, event: rio.SwitchChangeEvent):
         GlobalOptions.is_on_lower = event.is_on
+        if GlobalOptions.is_on_lower:
+            GlobalOptions.symbols += Symbols.LOWERCASE
+            return GlobalOptions.symbols
+        else:
+            GlobalOptions.symbols = delete_symbols(GlobalOptions.symbols,
+                                                   Symbols.LOWERCASE)
+            return GlobalOptions.symbols
 
     def is_on_change_upper(self, event: rio.SwitchChangeEvent):
         GlobalOptions.is_on_upper = event.is_on
+        if GlobalOptions.is_on_upper:
+            GlobalOptions.symbols += Symbols.UPPERCASE
+            return GlobalOptions.symbols
+        else:
+            GlobalOptions.symbols = delete_symbols(GlobalOptions.symbols,
+                                                   Symbols.UPPERCASE)
+            return GlobalOptions.symbols
 
     def is_on_change_nums(self, event: rio.SwitchChangeEvent):
         GlobalOptions.is_on_nums = event.is_on
+        if GlobalOptions.is_on_nums:
+            GlobalOptions.symbols += Symbols.DIGITS
+            return GlobalOptions.symbols
+        else:
+            GlobalOptions.symbols = delete_symbols(GlobalOptions.symbols,
+                                                   Symbols.DIGITS)
+            return GlobalOptions.symbols
 
     def is_on_change_symbols(self, event: rio.SwitchChangeEvent):
         GlobalOptions.is_on_symbols = event.is_on
+        if GlobalOptions.is_on_symbols:
+            GlobalOptions.symbols += Symbols.PUNCTUATION
+            return GlobalOptions.symbols
+        else:
+            GlobalOptions.symbols = delete_symbols(GlobalOptions.symbols,
+                                                   Symbols.PUNCTUATION)
+            return GlobalOptions.symbols
 
     def is_on_change_bad_symbol(self, event: rio.CheckboxChangeEvent):
         GlobalOptions.is_on_bad_symbol = event.is_on
+        if GlobalOptions.is_on_bad_symbol:
+            GlobalOptions.symbols = delete_bad_symbols(GlobalOptions.symbols)
+            return GlobalOptions.symbols
+        else:
+            GlobalOptions.symbols += Symbols.BAD_SYMBOLS
 
     def build(self) -> rio.Component:
         return rio.Grid(
