8a8f06f6b6f4828de99076643896e50a8a7ccc61
Eduard Gumen <hydrospirt@mail.ru>
2024-08-04 17:57:30 +0300

    Delete Complexity, GlobalComplexity

--- password-generator/password_generator/components/__init__.py
+++ password-generator/password_generator/components/__init__.py
@@ -1,6 +1,5 @@
 
 from .gen_placeholder import GenPlaceholder # noqa
 from .options import Options, GlobalOptions # noqa
-from .complexity import Complexity, GlobalComplexity # noqa
 from .title import Title # noqa
-from .footer import Footer
+from .footer import Footer # noqa
