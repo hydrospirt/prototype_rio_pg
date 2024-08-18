a105761447c3e030f6a359968d159138e030d22c
Eduard Gumen <hydrospirt@mail.ru>
2024-08-18 17:36:58 +0300

    Add rio.Card, change color and corner radius

--- password_generator/components/title.py
+++ password_generator/components/title.py
@@ -7,27 +7,31 @@ class Title(rio.Component):
     subtitle: str = HeaderSettings.SUB_TITLE
 
     def build(self) -> rio.Component:
-        return rio.Column(
-            rio.Text(
-                self.title,
-                justify="center",
-                style=rio.TextStyle(
-                    font_size=5,
-                    italic=True,
-                    font_weight="bold",
-                    fill=rio.LinearGradientFill(
-                        (self.session.theme.secondary_color, 0),
-                        (self.session.theme.primary_color, 1),
-                    ),
-                ),),
-            rio.Text(
-                self.subtitle,
-                justify="center",
-                style=rio.TextStyle(
-                     font_size=1,
-                     italic=True,
-                     fill=rio.LinearGradientFill(
-                        (self.session.theme.secondary_color, 0),
-                        (self.session.theme.primary_color, 1),),),
+        return rio.Card(
+            rio.Column(
+                rio.Text(
+                    self.title,
+                    justify="center",
+                    style=rio.TextStyle(
+                        font_size=5,
+                        italic=True,
+                        font_weight="bold",
+                        fill=rio.LinearGradientFill(
+                            (self.session.theme.secondary_color, 0),
+                            (self.session.theme.primary_color, 1),
+                        ),
+                    ),),
+                rio.Text(
+                    self.subtitle,
+                    justify="center",
+                    style=rio.TextStyle(
+                        font_size=1,
+                        italic=True,
+                        fill=rio.LinearGradientFill(
+                            (self.session.theme.secondary_color, 0),
+                            (self.session.theme.primary_color, 1),),),
+                ),
             ),
+            color=rio.Color.from_hex("#0A1819"),
+            corner_radius=(10, 5, 0, 0)
         )
