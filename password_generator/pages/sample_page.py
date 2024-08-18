127550c81ff0805242a164842f3d505c5acda656
Eduard Gumen <hydrospirt@mail.ru>
2024-08-18 17:36:07 +0300

    Delete Spacer

--- password_generator/pages/sample_page.py
+++ password_generator/pages/sample_page.py
@@ -12,9 +12,7 @@ class SamplePage(rio.Component):
         return rio.Column(
             comps.Title(),
             comps.GenPlaceholder(),
-            rio.Spacer(height=4.5),
             comps.Footer(),
-            spacing=2,
             margin=2,
             align_x=0.5,
             align_y=0,
