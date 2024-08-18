27a6fa4decf146dc88d7b4525b94e719b45cb10a
Eduard Gumen <hydrospirt@mail.ru>
2024-08-18 21:42:31 +0300

    Add Rectangle, change margins and spacing

--- password_generator/components/footer.py
+++ password_generator/components/footer.py
@@ -1,4 +1,5 @@
 import rio
+from pathlib import Path
 from ..utils import FooterSettings
 
 
@@ -10,19 +11,98 @@ class Footer(rio.Component):
     phone: str = FooterSettings.PHONE
 
     def build(self) -> rio.Component:
+        GRAD = rio.LinearGradientFill((self.session.theme.secondary_color, 0),
+                                      (self.session.theme.primary_color, 1),)
         return rio.Card(
-            rio.Grid(
-                rio.Text(self.copyright),
-                rio.Row(
-                    rio.Link(content=self.phone,
-                             target_url=self.target_link),
-                    rio.Link(content=self.email,
-                             target_url=self.target_link),
-                    rio.Link(content=self.adress,
-                             target_url=self.target_link),),
-                row_spacing=0.5,
-                margin=1,
-            ),
-            color=rio.Color.from_hex("#0A1819"),
-            corner_radius=(0, 0, 10, 0)
-        )
+                rio.Grid(
+                    rio.Rectangle(content=rio.Grid(
+                        rio.Row(rio.Spacer(),
+                                rio.Card(rio.Row(
+                                    rio.Icon(
+                                        'material/phone_forwarded',
+                                        width=0.2,
+                                        height=0.2,
+                                        fill=GRAD
+                                    ),
+                                    rio.Column(
+                                        rio.Text("Call Me",
+                                                 style=rio.TextStyle(
+                                                     font_size=1,
+                                                     font_weight="bold",
+                                                 )),
+                                        rio.Link(content=self.phone,
+                                                 target_url=self.target_link,),),
+                                proportions=[1, 5],),
+                                corner_radius=(1, 1, 1, 1),
+                                height=2,
+                                colorize_on_hover=True,
+                                color=rio.Color.from_hex("#11292B"),),
+                                proportions=[1, 1],
+                                margin_right=7,),
+                        rio.Row(rio.Spacer(),
+                                rio.Card(rio.Row(
+                                    rio.Icon("material/mail",
+                                        width=0.2,
+                                        height=0.2,
+                                        fill=GRAD),
+                                    rio.Column(
+                                        rio.Text("Write Me",
+                                                 style=rio.TextStyle(
+                                                     font_size=1,
+                                                     font_weight="bold",
+                                                 )),
+                                        rio.Link(content=self.email,
+                                                 target_url=self.target_link),),
+                                proportions=[1, 5]
+                                ),
+                                corner_radius=(1, 1, 1, 1),
+                                height=2,
+                                colorize_on_hover=True,
+                                color=rio.Color.from_hex("#11292B"),),
+                                proportions=[1, 1],
+                                margin_right=7,),
+                        rio.Row(rio.Spacer(),
+                                rio.Card(rio.Row(
+                                    rio.Icon(
+                                        'material/location_city',
+                                        width=0.2,
+                                        height=0.2,
+                                        fill=GRAD
+                                    ),
+                                    rio.Column(
+                                        rio.Text("Find Me",
+                                                 style=rio.TextStyle(
+                                                     font_size=1,
+                                                     font_weight="bold",
+                                                 )),
+                                        rio.Link(content=self.adress,
+                                                 target_url=self.target_link),),
+                                    proportions=[1, 4.5]
+                                    ),
+                                corner_radius=(1, 1, 1, 1),
+                                height=2,
+                                colorize_on_hover=True,
+                                color=rio.Color.from_hex("#11292B"),),
+                                proportions=[1, 1],
+                                margin_right=7,),
+                        row_spacing=1,
+                        margin_bottom=1,
+                        margin_top=1,
+                        ),
+                        fill=rio.ImageFill(Path(r"D:\Dev\prototype_rio_pg\password_generator\assets\fun-footer.jpeg"),
+                                        fill_mode="zoom",),
+                        corner_radius=(0, 0, 10, 0),),
+                    rio.Separator(margin_right=5),
+                    rio.Text(self.copyright,
+                            margin_left=1,
+                            style=rio.TextStyle(
+                                italic=True,
+                                fill=rio.LinearGradientFill(
+                                    (self.session.theme.secondary_color, 0),
+                                    (self.session.theme.primary_color, 1),),),),
+                    row_spacing=0.5,
+                    margin=1,
+                ),
+                color=rio.Color.from_hex("#0A1819"),
+                corner_radius=(0, 0, 10, 0)
+            )
