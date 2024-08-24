a70bea4708e8f6fe366eac5ed060b9a21f4a4a09
Eduard Gumen <hydrospirt@mail.ru>
2024-08-24 20:12:08 +0300

    Add Rectangle and background art

--- password_generator/pages/sample_page.py
+++ password_generator/pages/sample_page.py
@@ -1,5 +1,5 @@
 import rio
-
+from pathlib import Path
 from .. import components as comps
 
 
@@ -9,11 +9,17 @@ class MainPage(rio.Component):
     """
 
     def build(self) -> rio.Component:
-        return rio.Column(
-            comps.Title(),
-            comps.GenPlaceholder(),
-            comps.Footer(),
-            margin=2,
-            align_x=0.5,
-            align_y=0,
+        return rio.Rectangle(
+            content=rio.Column(
+                comps.Title(),
+                comps.GenPlaceholder(),
+                comps.Footer(),
+                margin=2,
+                align_x=0.5,
+                align_y=0,
+            ),
+            fill=rio.ImageFill(
+                Path(self.session.assets / "fun-footer.jpeg"),
+                fill_mode="fit"
+            ),
         )
