335c25cea0fcc9b4b672f19343f64d0abb6c27f5
Eduard Gumen <hydrospirt@mail.ru>
2024-08-30 22:34:52 +0300

    Add MainPageSettings

--- password_generator/utils/__init__.py
+++ password_generator/utils/__init__.py
@@ -1,2 +1,2 @@
 from .utils import create_new_password, Symbols, delete_bad_symbols, delete_symbols, get_password_entropy # noqa
-from .settings import FooterSettings, HeaderSettings, OPTIONS_TEXT # noqa
+from .settings import FooterSettings, HeaderSettings, OPTIONS_TEXT, MainPageSettings # noqa
